1df3c6871f4227eac40f69b15ab5bca57c157631
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-12-17 18:14:42 +0000

    sfs

--- CADASTRO-FUNCIONARIOS - Copia-20241211T062111Z-001/CADASTRO-FUNCIONARIOS - Copia/src/main/java/com/mycompany/cadastro/funcionarios/BancoDeDados.java
+++ CADASTRO-FUNCIONARIOS - Copia-20241211T062111Z-001/CADASTRO-FUNCIONARIOS - Copia/src/main/java/com/mycompany/cadastro/funcionarios/BancoDeDados.java
@@ -1,6 +1,7 @@
 package com.mycompany.cadastro.funcionarios;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 final class BancoDeDados {
     static ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
@@ -27,6 +28,51 @@ final class BancoDeDados {
         
         }
         return soma;
+
+    }
+
+    static public void modificarFuncionario(){
+        for (Funcionario funcionario : funcionarios) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Digite o nome do Funcionário.");
+            String nome = sc.nextLine();
+            if(funcionario.getNome().equals(nome)){
+                System.out.println("#########################");
+                System.out.println("-Que campo deseja mudar?");
+                System.out.println("01-Nome.");
+                System.out.println("02-Salario.");
+                System.out.println("03-Cargo.");
+                System.out.println("04-Remover.");
+                System.out.println("#########################");
+                int opcao = sc.nextInt();
+
+                switch(opcao){
+                    case 1:
+                        sc.nextLine();
+                        System.out.println("Digite o novo nome: ");
+                        String nom = sc.nextLine();
+                        funcionario.setNome(nom);
+                        break;
+                    case 2:
+                        double sal = sc.nextDouble();
+                        funcionario.setSalario(sal);
+                        break;
+                    case 3:
+                        String car = sc.nextLine();
+                        funcionario.setCargo(car);
+                        break;
+                    case 4:
+                        funcionarios.remove(funcionario);
+                        break;
+                }
+
+            }else{
+                System.out.println("Funcionario não encontrado");
+            }
+        
+        }
+
         
     }
+    
 }
--- CADASTRO-FUNCIONARIOS - Copia-20241211T062111Z-001/CADASTRO-FUNCIONARIOS - Copia/src/main/java/com/mycompany/cadastro/funcionarios/CADASTROFUNCIONARIOS.java
+++ CADASTRO-FUNCIONARIOS - Copia-20241211T062111Z-001/CADASTRO-FUNCIONARIOS - Copia/src/main/java/com/mycompany/cadastro/funcionarios/CADASTROFUNCIONARIOS.java
@@ -21,10 +21,12 @@ public class CADASTROFUNCIONARIOS {
         boolean menu = true;
         while (menu){
             System.out.println("#########################");
+            System.out.println("#######Bem-vindo!########");
             System.out.println("01-Cadastrar funcíonário.");
             System.out.println("02-Listar funcíonários.");
             System.out.println("03-Folha de pagamentos.");
-            System.out.println("04-Sair.");
+            System.out.println("04-Atualizar dados.");
+            System.out.println("05-Sair.");
             System.out.println("#########################");
             int opcao = sc.nextInt();
             switch (opcao) {
@@ -37,12 +39,17 @@ public class CADASTROFUNCIONARIOS {
                 case 3:
                     System.out.println(BancoDeDados.folhaPagamento());
                     break;
+                case 4:
+                    BancoDeDados.modificarFuncionario();
+                    break;
+                case 5:
+                    System.out.println("Até breve!");
+                    menu = false;
+                    break;
             }
 
         }
         
     }
         
-
-
 }
